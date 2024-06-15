36bfb5afbf99b35c2d4c6beb45ed1571aa027211
mukesh kumar chandra <mukeshc@theblackthreat.in>
2024-06-15 12:01:29 +0530

    add file of learing java script

---
+++ java.js
@@ -0,0 +1,38 @@
+function exampal(){
+    if(true){
+       let rc= "hello world";
+
+       console.log(rc);
+       console.log(typeof rc);
+
+    }
+}exampal();
+
+document.write("ram ram");
+
+let a = "ram narayan";
+let b ="";
+
+console.log(a,b);
+console.log(typeof b);
+console.log(typeof a);
+
+let x = 20<10;
+
+console.log(x);
+console.log(typeof x);
+
+let number= null;
+
+console.log(number);
+console.log(typeof number);
+
+let person = {
+    firstname : "mukesh",
+    midname : 'kumar',
+    lastname: 'chandra',
+    age : '21',
+};
+
+    console.log(person);
+    console.log(typeof person)
